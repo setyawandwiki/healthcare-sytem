633d18a78f0a7fef68e4404a3254e4e7f5a8628d
setyawandwiki <setyawandwiki1@gmail.com>
2025-02-23 13:23:34 +0700

    adding appointment entity

---
+++ healthcare-project/src/main/java/com/STWN/healthcare_project/entity/Appointment.java
@@ -0,0 +1,64 @@
+package com.STWN.healthcare_project.entity;
+
+import jakarta.persistence.*;
+import lombok.AllArgsConstructor;
+import lombok.Builder;
+import lombok.Data;
+import lombok.NoArgsConstructor;
+import org.hibernate.annotations.CreationTimestamp;
+import org.hibernate.annotations.UpdateTimestamp;
+
+import java.time.LocalDate;
+import java.time.LocalDateTime;
+import java.time.LocalTime;
+
+@Entity
+@Table(name = "appointment")
+@Data
+@Builder
+@AllArgsConstructor
+@NoArgsConstructor
+public class Appointment {
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    private Long id;
+
+    @Column(name = "patient_id", nullable = false)
+    private Long patientId;
+
+    @Column(name = "doctor_id", nullable = false)
+    private Long doctorId;
+
+    @Column(name = "hospital_id", nullable = false)
+    private Long hospitalId;
+
+    @Column(name = "meeting_id")
+    private String meetingId;
+
+    @Column(name = "doctor_specialization_id", nullable = false)
+    private Long doctorSpecializationId;
+
+    @Column(name = "appointment_date", nullable = false)
+    private LocalDate appointmentDate;
+
+    @Column(name = "start_time", nullable = false)
+    private LocalTime startTime;
+
+    @Column(name = "end_time", nullable = false)
+    private LocalTime endTime;
+
+    @Column(name = "consultation_type", nullable = false, length = 10)
+    private String consultationType;
+
+    @Column(nullable = false, length = 20)
+    @Enumerated(EnumType.STRING)
+    private AppointmentStatus status;
+
+    @CreationTimestamp
+    @Column(name = "created_at", nullable = false, updatable = false)
+    private LocalDateTime createdAt;
+
+    @UpdateTimestamp
+    @Column(name = "updated_at", nullable = false)
+    private LocalDateTime updatedAt;
+}
---
+++ healthcare-project/src/main/java/com/STWN/healthcare_project/entity/AppointmentStatus.java
@@ -0,0 +1,5 @@
+package com.STWN.healthcare_project.entity;
+
+public enum AppointmentStatus {
+    SCHEDULED, COMPLETED, CANCELLED, NO_SHOW
+}
